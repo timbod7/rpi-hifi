4f997678c2079aabb6142adccf80f106cd48b020
Tim Docker <tim@dockerz.net>
2023-05-29 16:37:24 +1000

    embedded graphics going on the OLED display

--- hifi-control/src/main.rs
+++ hifi-control/src/main.rs
@@ -1,3 +1,65 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use rppal::i2c::I2c;
+
+use embedded_graphics::{
+    pixelcolor::BinaryColor,
+    prelude::*,
+    primitives::{Circle, PrimitiveStyleBuilder, Rectangle, Triangle},
+};
+
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+
+fn main() -> Result<(), Box<dyn Error>>  {
+
+    let i2c = I2c::new()?;
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    println!("Initialized i2c and display.");
+    
+    let yoffset = 20;
+
+    let style = PrimitiveStyleBuilder::new()
+        .stroke_width(1)
+        .stroke_color(BinaryColor::On)
+        .build();
+
+    // screen outline
+    // default display size is 128x64 if you don't pass a _DisplaySize_
+    // enum to the _Builder_ struct
+    Rectangle::new(Point::new(0, 0), Size::new(127, 63))
+        .into_styled(style)
+        .draw(&mut display)
+        .unwrap();
+
+    // triangle
+    Triangle::new(
+        Point::new(16, 16 + yoffset),
+        Point::new(16 + 16, 16 + yoffset),
+        Point::new(16 + 8, yoffset),
+    )
+    .into_styled(style)
+    .draw(&mut display)
+    .unwrap();
+
+    // square
+    Rectangle::new(Point::new(52, yoffset), Size::new_equal(16))
+        .into_styled(style)
+        .draw(&mut display)
+        .unwrap();
+
+    // circle
+    Circle::new(Point::new(88, yoffset), 16)
+        .into_styled(style)
+        .draw(&mut display)
+        .unwrap();
+
+    display.flush().unwrap();
+
+    println!("Drawn some stuff");
+
+    Ok(())
 }
