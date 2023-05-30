d17aca4d2ee24578faa207bf3ecdcfe92e81a97c
Tim Docker <tim@dockerz.net>
2023-05-31 09:32:02 +1000

    keep the displayed time up to date

--- hifi-control/src/main.rs
+++ hifi-control/src/main.rs
@@ -1,19 +1,22 @@
-use std::error::Error;
-
 use embedded_graphics::prelude::*;
+use chrono::prelude::*;
 
 use display::Display;
 
 mod display;
 
-fn main() -> Result<(), Box<dyn Error>>  {
+fn main() -> ()  {
 
     let mut dpy = Display::new();
 
-    dpy.render_text("13:48", Point::new(0, 0));
     dpy.render_text("Cpu 10%", Point::new(72, 0));
     dpy.render_rect(Point::new(0, 40), Size::new(127, 1));
     dpy.target.flush().unwrap();
 
-    Ok(())
+    loop {
+        let now = Local::now();
+        dpy.render_text(&format!("{:0>2}:{:0>2}", now.hour(), now.minute()), Point::new(0, 0));
+        dpy.target.flush().unwrap();
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
 }
