3b3f5c8cff87bed7c0c533ec2aeb5527df3562bf
Tim Docker <tim@dockerz.net>
2023-05-31 06:05:09 +1000

    initial screen content layout

--- hifi-control/src/main.rs
+++ hifi-control/src/main.rs
@@ -5,7 +5,7 @@ use rppal::i2c::I2c;
 use embedded_graphics::{
     pixelcolor::BinaryColor,
     prelude::*,
-    primitives::{Circle, PrimitiveStyleBuilder, Rectangle, Triangle},
+    primitives::{PrimitiveStyleBuilder, Rectangle}, mono_font::MonoTextStyleBuilder, text::{TextStyle, Baseline, Text},
 };
 
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
@@ -18,48 +18,37 @@ fn main() -> Result<(), Box<dyn Error>>  {
         .into_buffered_graphics_mode();
     display.init().unwrap();
 
-    println!("Initialized i2c and display.");
-    
-    let yoffset = 20;
+
+    let char_style = MonoTextStyleBuilder::new()
+        .font(&profont::PROFONT_12_POINT)
+        .text_color(BinaryColor::On)
+        .background_color(BinaryColor::Off)
+        .build();
+    let text_style = TextStyle::with_baseline(Baseline::Top);
+
 
     let style = PrimitiveStyleBuilder::new()
         .stroke_width(1)
         .stroke_color(BinaryColor::On)
         .build();
 
+    Text::with_text_style("12:43", Point::new(0, 0), char_style, text_style)
+        .draw(&mut display)
+        .unwrap();
+
+    Text::with_text_style("Cpu 13%", Point::new(72, 0), char_style, text_style)
+        .draw(&mut display)
+        .unwrap();
+
     // screen outline
     // default display size is 128x64 if you don't pass a _DisplaySize_
     // enum to the _Builder_ struct
-    Rectangle::new(Point::new(0, 0), Size::new(127, 63))
-        .into_styled(style)
-        .draw(&mut display)
-        .unwrap();
-
-    // triangle
-    Triangle::new(
-        Point::new(16, 16 + yoffset),
-        Point::new(16 + 16, 16 + yoffset),
-        Point::new(16 + 8, yoffset),
-    )
-    .into_styled(style)
-    .draw(&mut display)
-    .unwrap();
-
-    // square
-    Rectangle::new(Point::new(52, yoffset), Size::new_equal(16))
-        .into_styled(style)
-        .draw(&mut display)
-        .unwrap();
-
-    // circle
-    Circle::new(Point::new(88, yoffset), 16)
+    Rectangle::new(Point::new(0, 40), Size::new(127, 1))
         .into_styled(style)
         .draw(&mut display)
         .unwrap();
 
     display.flush().unwrap();
-
-    println!("Drawn some stuff");
 
     Ok(())
 }
